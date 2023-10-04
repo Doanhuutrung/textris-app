f03b963d6bae30901dfa3d8ffb712ae51702f35a
Bond <bond.fastcodingvn@gmail.com>
2023-10-04 17:29:05 +0700

    [upd] stage's component and add helper

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { createStage } from "../gameHelper";
 //components
 import Stage from "./Stage";
 import Display from "./Display";
@@ -8,7 +8,7 @@ import StartButton from "./StartButton";
 const Tetris = () => {
   return(
   <div>
-    <Stage />
+    <Stage stage={createStage()}/>
     <aside>
       <div>
         <Display text="Score" />
